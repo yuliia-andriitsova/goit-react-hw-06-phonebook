c36e28c9b0e916e426f2d2b55f4b8e9b4794ac7b
yuliia-andriitsova <andriitsovayulia@gmail.com>
2022-11-04 20:50:14 +0200

    delete action

--- src/redux/phonebook/phonebookSlice.js
+++ src/redux/phonebook/phonebookSlice.js
@@ -24,25 +24,3 @@ export const { addContactAction, onDeleteContactAction } =
   phonebookSlice.actions;
 
 export default phonebookSlice.reducer;
-
-// export const contactsReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case addContact:
-//       return { ...state, contacts: [...state.contacts, action.payload] };
-//     // return { contacts: [...state.contacts, action.payload] };
-
-//     case changeFilter:
-//       //   return { ...state, contacts: [...state.contacts, action.payload] };
-//       return { ...state, filter: action.payload };
-
-//     case deleteContact:
-//       return {
-//         ...state,
-//         contacts: state.contacts.filter(
-//           contact => contact.id !== action.payload
-//         ),
-//       };
-//     default:
-//       return state;
-//   }
-// };
